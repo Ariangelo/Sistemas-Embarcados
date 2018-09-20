c57762b7197b577aac200364797e1545173d5f09
Ariangelo <ariangelo@gmail.com>
2018-09-20 11:10:47 -0300

    Codigo fonte ESP8266 - Arduino IDE

--- ESP8266/Arduino IDE/Controle_MUX_BTN/Controle_MUX_BTN.ino
+++ ESP8266/Arduino IDE/Controle_MUX_BTN/Controle_MUX_BTN.ino
@@ -1,12 +1,21 @@
 #define PIN_BOTAO1  0
-#define pinoMUX     2
+#define PIN_BOTAO2  2
+#define pinoMUX     15
 #define PIN_S0      12
 #define PIN_S1      13
 #define PIN_S2      14
 
-int statusBotao;
-int statusBotaoAnterior = LOW;
-unsigned long debounceAnterior = 0;
+typedef struct // Cria uma STRUCT para armazenar os dados dos botoes
+{
+    int pino; 
+    int canal = -1;
+    int statusBotao;
+    int statusBotaoAnterior = LOW;    
+    unsigned long debounceAnterior = 0;
+} Botao;
+
+Botao botao1;
+Botao botao2;
 
 int val[8] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, HIGH};
 
@@ -21,29 +30,42 @@ void muxWrite(int canal) {
 }
 
 void setup() {
+  Serial.begin(115200);
+  botao1.pino = PIN_BOTAO1;
+  botao1.canal = 0;
+  botao2.pino = PIN_BOTAO2;
+  botao2.canal = 3;
+  pinMode(botao1.pino, INPUT_PULLUP);
+  pinMode(botao2.pino, INPUT_PULLUP);
   pinMode(pinoMUX, OUTPUT);
 }
 
 void loop() {
-  debounce(PIN_BOTAO1);
+  botao1 = debounce(botao1);
+  botao2 = debounce(botao2);
   for (int i = 0; i < 8; i++) {
     muxWrite(i);
     delay(1);
   }
 }
 
-void debounce(int b) {
-  int leitura = digitalRead(b);
-  if (leitura != statusBotaoAnterior) {
-    debounceAnterior = millis();
+Botao debounce(Botao b) {
+  int leitura = digitalRead(b.pino);
+  if (leitura != b.statusBotaoAnterior) {
+    b.debounceAnterior = millis();
   }
-  if ((millis() - debounceAnterior) > 50) {
-    if (leitura != statusBotao) {
-      statusBotao = leitura;
-      if (statusBotao == HIGH) {
-        val[0] = !val[0];
+  if ((millis() - b.debounceAnterior) > 50) {
+    if (leitura != b.statusBotao) {
+      b.statusBotao = leitura;
+      if (b.statusBotao == HIGH) {
+        val[b.canal] = !val[b.canal];
+        Serial.print("Canal: ");
+        Serial.print(b.canal);
+        Serial.print(" = ");
+        Serial.println(val[b.canal]);
       }
     }
   }
-  statusBotaoAnterior = leitura;
+  b.statusBotaoAnterior = leitura;
+  return b;
 }
