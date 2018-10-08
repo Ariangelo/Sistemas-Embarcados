0170412940cbd254dc0ac14d35812b2e9e221ed3
Ariangelo <ariangelo@gmail.com>
2018-10-08 09:52:29 -0300

    Update PWM_fade.ino

--- ESP8266/Arduino IDE/PWM_fade/PWM_fade.ino
+++ ESP8266/Arduino IDE/PWM_fade/PWM_fade.ino
@@ -1,7 +1,7 @@
 #define led 14
 
-int brilho = 0; 
-int valorFade = 1;
+int brilho = 0;     // valor inicial para o brilho do LED
+int valorFade = 1;  // valor de aumento do brilho do LED
 
 void setup() {
   pinMode(led, OUTPUT);
@@ -9,9 +9,12 @@ void setup() {
 
 void loop() {
   analogWrite(led, brilho);
+  // atualiza o valor do brilho do LED:
   brilho = brilho + valorFade;
+  // inverte a forma do fade:
   if (brilho == 0 || brilho == 255) {
     valorFade = -valorFade ;
   }
+  // aguarda 30 milissegundos para ver o efeito de fade
   delay(30);
 }
