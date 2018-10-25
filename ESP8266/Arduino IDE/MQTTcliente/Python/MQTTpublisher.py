0fa420a46d8d9e65c8537b315b03bc0bacf6fe90
Ariangelo <ariangelo@gmail.com>
2018-10-25 10:31:43 -0300

    Atualizações

---
+++ ESP8266/Arduino IDE/MQTTcliente/Python/MQTTpublisher.py
@@ -0,0 +1,37 @@
+#
+# Exemplo MQTT publisher usando paho-mqtt
+#
+
+import sys
+import time
+import paho.mqtt.client as mqtt
+
+#definicoes: 
+broker = "broker.mqtt-dashboard.com"
+portaBroker = 1883
+topico = 'Sistemas.Embarcados.Topico.Entrada' #dica: o nome do topico deve ser "unico", 
+
+#Callback - conexao ao broker realizada
+def on_connect(client, userdata, flags, rc):
+    print("[STATUS] Conectado ao Broker. Resultado de conexao: " + str(rc))
+   
+#programa principal:
+def main():
+    try:
+        client = mqtt.Client()    
+        client.on_connect = on_connect
+        client.connect(broker, portaBroker)
+        client.loop_start()
+        seq = 1
+        while True:
+            payload = '1' if seq % 2 == 0 else '0'
+            client.publish(topico, payload)
+            print('evento publicado: payload= {}'.format(payload))
+            seq += 1
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("\nCtrl+C pressionado, encerrando aplicacao e saindo...")
+        sys.exit(0)
+    
+if __name__ == '__main__':
+    main()    
