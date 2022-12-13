2fc7306c142f290865d80869eeb9775b0941b5ee
Ariangelo <ariangelo@gmail.com>
2022-12-13 13:56:13 -0300

    Update config.h

--- ESP8266/Arduino IDE/andarilho/andarilho/arduino/config.h
+++ ESP8266/Arduino IDE/andarilho/andarilho/arduino/config.h
@@ -2,8 +2,21 @@
 #define Config
 
 #include <Arduino.h>
+#include <WiFiManager.h>  // https://github.com/tzapu/WiFiManager
+#include <PubSubClient.h>
 #include <Arduino_JSON.h>
+#include <Ticker.h>  //para controlar estado do LED
 #include <Ultrasonic.h>
+
+#ifdef ESP32
+#include <BluetoothSerial.h>
+#else
+#include <SoftwareSerial.h>
+#endif
+
+#if defined(ESP32) && (!defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED))
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 22  // ESP32 DOES NOT DEFINE LED_BUILTIN
@@ -12,23 +25,25 @@
 #define BT_NAME "Andarilho"
 #define NUM_MOTORS 2
 
-#define PWM_A 14      // D5
-#define PWM_B 0       // D3
-#define AI1 13        // D7
-#define AI2 12        // D6
-#define BI1 5         // D1
-#define BI2 4         // D2
-#define TRIGGER_PIN 2 // D4
-#define ECHO_PIN 15   // D8
+#define PWM_A 14
+#define PWM_B 0
+#define AI1 13
+#define AI2 12
+#define BI1 5
+#define BI2 4
+#define TRIGGER_PIN 2
+#define ECHO_PIN 15
 
 enum Source {
   BT,
   MQTT
 };
-
 extern JSONVar actions;
-bool cabec(Stream& port);
 
+bool cabec(Stream& port);
+bool wifiConnect();
+bool mqttConnect(PubSubClient& clienteMQTT, char* server, char* mqttClient, int port, char* mqttTopic);
+int connectMQTT(PubSubClient& clienteMQTT);
 JSONVar createActions(Source source, String payload);
 
 #endif  // Config
